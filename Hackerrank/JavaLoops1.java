53628252e3acf8117bd93c47d9dce307b3a7e690
Lucas Lima <lucas_soalima@hotmail.com>
2022-09-29 17:11:59 -0300

    Exercicios Logica Hackerrank Adicionados

---
+++ Hackerrank/JavaLoops1.java
@@ -0,0 +1,40 @@
+package Hackerrank;
+
+// In this challenge, we're going to use loops to help us do some simple math.
+
+// Task
+// Given an integer, N, print its first 10 multiples. Each multiple N x i (where 1 <= i <= 10) should be printed on a new line in the form: N x i = result.
+
+// Input Format
+
+// A single integer, N.
+
+// Constraints
+// 2 <= N <= 20
+// Output Format
+
+// Print 10 lines of output; each line i (where 1 <= i <= 10) contains the result of N in the form:
+// N x i = result.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        int i = 1;
+        int soma;
+        
+        bufferedReader.close();
+        
+        while(i <= 10){
+            soma = N * i;
+            System.out.println(N + " x " + i + " = " + soma);
+            i++;
+        }
+    }
+}
+
---
+++ Hackerrank/StdinStdoutChallenge.java
@@ -0,0 +1,38 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+// In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
+
+// Note: We recommend completing Java Stdin and Stdout I before attempting this challenge.
+
+// Input Format
+
+// There are three lines of input:
+
+// The first line contains an integer.
+// The second line contains a double.
+// The third line contains a String.
+// Output Format
+
+// There are three lines of output:
+
+// On the first line, print String: followed by the unaltered String read from stdin.
+// On the second line, print Double: followed by the unaltered double read from stdin.
+// On the third line, print Int: followed by the unaltered integer read from stdin.
+
+public class StdinStdoutChallenge {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int i = entrada.nextInt();
+        double d = entrada.nextDouble();
+        entrada.nextLine();
+        String myString = entrada.nextLine();
+        
+        System.out.println("String: " + myString);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+}
