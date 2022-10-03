21ae05920300b380b39b566204aecf206abf8c71
Lucas Lima <lucas_soalima@hotmail.com>
2022-10-03 13:36:22 -0300

    Java Loops Challenge 2

---
+++ Hackerrank/JavaLoops2.java
@@ -0,0 +1,25 @@
+package Hackerrank;
+ 
+import java.util.Scanner;
+
+public class JavaLoops2 {
+
+public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int result = a + (int) Math.pow(2, 0) * b;
+            System.out.print(result + " ");
+            for(int j=1; j<n; j++){
+                result += (int) Math.pow(2, j) * b;
+                System.out.print(result + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+
+    }
+}
